ea1b86d135a239a52c7e5f908e6e94dea8828702
kinamukumba <kinamukumba@gmail.com>
2026-01-06 13:22:43 -0300

    Set function click_link

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,49 @@
+/*Open menu mobile*/
 const optionMenu = document.querySelectorAll(
   ".content-menu .close-menu i," + ".content-header .menu-mobile i"
 );
-if (optionMenu) {
+var menu = document.querySelector(".content-menu");
+if (optionMenu && menu) {
   console.log(optionMenu);
-  const menu = document.querySelector(".content-menu");
+
   optionMenu[0].onclick = () => {
     menu.classList.add("open");
-    console.log('ola');
-    
+    console.log("ola");
   };
   optionMenu[1].onclick = () => {
     menu.classList.remove("open");
   };
 }
 
+const clinksClicked = document.querySelectorAll(
+  ".content-menu .list-itens-nav li a"
+);
+if (clinksClicked) {
+  console.log(clinksClicked);
+  clinksClicked.forEach((link) => {
+    link.onclick = () => {
+      menu.classList.remove("open");
+    };
+  });
+}
 
 /*Make links*/
-const authPath = document.querySelectorAll('.itens-btn li button,' + '.content-menu .itens-btn li button');
-if(authPath){
-  console.log(authPath);
-  
-  authPath[0].onclick = ()=>{
-    window.location.href = './auth/login.html'
-  }
-  authPath[1].onclick = ()=>{
-    window.location.href = './auth/sign-up.html'
-  }
-  authPath[2].onclick = ()=>{
-    window.location.href = './auth/login.html'
-  }
-  authPath[3].onclick = ()=>{
-    window.location.href = './auth/sign-up.html'
-  }
+const authPath = document.querySelectorAll(
+  ".itens-btn li button," + ".content-menu .itens-btn li button"
+);
+if (authPath) {
+  authPath[0].onclick = () => {
+    window.location.href = "./auth/login.html";
+  };
+  authPath[1].onclick = () => {
+    window.location.href = "./auth/sign-up.html";
+  };
+  authPath[2].onclick = () => {
+    window.location.href = "./auth/login.html";
+    menu.classList.remove("open");
+  };
+  authPath[3].onclick = () => {
+    window.location.href = "./auth/sign-up.html";
+    menu.classList.remove("open");
+  };
 }
